a593bb037e388260cbf433217b82f853539231fc
Sachiyo <sachiyo.sable@gmail.com>
2025-04-02 08:52:33 +0200

    modified .env & connected DB

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { loginUser, registerUser }= require('../controllers/userController');
 
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
